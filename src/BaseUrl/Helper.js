4222f656480855ea58affe60af58ecc236311028
ayantufaill <ayantufail.tdc@gmail.com>
2023-12-27 18:25:18 +0500

    first commit

--- src/BaseUrl/Helper.js
+++ src/BaseUrl/Helper.js
@@ -1,7 +1,7 @@
 export const Helpers = {
-    AuthTokken: '',
-    CarBrandName:'' ,
-    SelectCarModel:'', 
-    FuelCapacity:'',
-    BaseUrlAutoNova:'http://192.168.1.215:3000'
+  AuthTokken: "",
+  CarBrandName: "",
+  SelectCarModel: "",
+  FuelCapacity: "",
+  BaseUrlAutoNova: "http://192.168.1.191:3000",
 };
